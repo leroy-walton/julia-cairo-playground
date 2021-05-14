4b2431159cedddfc83d266f045377dc5d74245c0
Leroy Walton <leroy.walton@protonmail.com>
2021-05-14 13:01:32 +0200

    Person Cast Movie structs

--- imdb-scraping.jl
+++ imdb-scraping.jl
@@ -1,26 +1,58 @@
 using Cascadia
 using Gumbo
 using HTTP
+using Serialization
+
 #using AbstractTrees
 #using DataFrames
 
+println("---")
+
+mutable struct Person
+    id::String
+    name::String
+    Person() = new()
+end
+function Base.:(==)(x::Person, y::Person) 
+    (x.name == y.name)
+end
+
+function Base.hash(x::Person, h::UInt )
+    hash(x.name)
+end
+
+mutable struct Cast
+    movie_id::String
+    director::Person
+    writers::Set{Person}
+    actors::Set{Person}
+    producers::Set{Person}
+    Cast() = new()
+end
+
 mutable struct Movie
-    id
-    title
-    year
-    rating
-    genre
-    time
-    director
-    writer
-    cast
+    id::String
+    title::String
+    year::String
+    rating::String
+    genre::String
+    time::String
+    cast::Cast
     Movie() = new()
 end
 
+function getMovie(id::String)
+    
+end
 
-println("---")
+#==================================================================================================#
 
-id = "tt0120586"
+movies = Set{Movie}()
+persons = Set{Person}()
+
+#id = "tt0120586" #american history x
+#id = "tt0114814" #the usual suspects
+id = "tt0083944" # rambo
 
 url = "https://www.imdb.com/title/$id"
 doc = parsehtml(String(HTTP.get(url).body))
@@ -37,4 +69,35 @@ movie.time = strip(eachmatch(sel"[datetime]", body)[1][1].text)
 
 println(movie)
 
+url = "https://www.imdb.com/title/$id/fullcredits/"
 
+doc = parsehtml(String(HTTP.get(url).body))
+body = doc.root[2]
+
+cast = Cast()
+
+table_rows  = eachmatch(sel".cast_list", body)[1].children[1]
+#cast_persons = []
+cast_persons = Set()
+s = size(table_rows.children)[1]
+println(s)
+for i in 2:s
+    try
+        print("$i ") 
+        person = Person()
+        person.name = strip(table_rows[i][2][1][1].text)
+        push!(cast_persons, person)
+        println(person.name)
+    catch e
+        println(" /!\\ no name found in this children")
+    end
+    
+end
+cast.actors = cast_persons
+
+movie.cast = cast
+movie
+
+serialize("/julia/serialised.test", movie)
+
+movieb = deserialize("/julia/serialised.test")
